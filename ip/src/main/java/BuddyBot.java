ff10a435e8390ee762c0c03b2d37e4fb8eeb381d
himethcodes <himeththewmika@gmail.com>
2025-02-07 21:29:53 +0800

    Level-0: Rename, Greet, Exit

---
+++ ip/src/main/java/BuddyBot.java
@@ -0,0 +1,11 @@
+public class BuddyBot { // Replace  chatbot's name
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm BuddyBot");  // Change  chatbot's name
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
