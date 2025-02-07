303dc880c00dfc47c279bf6c8d25d2f637c9d752
himethcodes <himeththewmika@gmail.com>
2025-02-07 22:34:22 +0800

    Level-1: Echo feature added

--- ip/src/main/java/BuddyBot.java
+++ ip/src/main/java/BuddyBot.java
@@ -1,11 +1,29 @@
-public class BuddyBot { // Replace  chatbot's name
+import java.util.Scanner;
+
+public class BuddyBot {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm BuddyBot");  // Change  chatbot's name
+        System.out.println(" Hello! I'm BuddyBot");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + userInput);
+            System.out.println("____________________________________________________________");
+        }
+
+        scanner.close();
     }
 }
