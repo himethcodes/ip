b9ae9380954e03fc980903b5bbeeb9ab0adfd9a6
Himeth Thewmika <142070705+himethcodes@users.noreply.github.com>
2025-04-01 17:50:35 +0800

    Update Todo.java

--- ip/src/main/java/Todo.java
+++ ip/src/main/java/Todo.java
@@ -1,10 +1,35 @@
-public class Todo extends Task {
-    public Todo(String description) {
+/**
+ * Represents a ToDo task.
+ */
+public class ToDo extends Task {
+
+    /**
+     * Constructs a new ToDo task.
+     *
+     * @param description Description of the task.
+     */
+    public ToDo(String description) {
         super(description);
+    }
+
+    /**
+     * Constructs a new ToDo task with a completion status.
+     * Used when loading from a file.
+     *
+     * @param description Description of the task.
+     * @param isDone Whether the task is completed.
+     */
+    public ToDo(String description, boolean isDone) {
+        super(description, isDone);
     }
 
     @Override
     public String toString() {
         return "[T]" + super.toString();
+    }
+
+    @Override
+    public String toFileFormat() {
+        return "T | " + (isDone ? "1" : "0") + " | " + description;
     }
 }
