88b9ad84ce40245ee874236284584837be0018d6
Himeth Thewmika <142070705+himethcodes@users.noreply.github.com>
2025-04-01 15:46:45 +0800

    Update Event.java

--- ip/src/main/java/Event.java
+++ ip/src/main/java/Event.java
@@ -1,15 +1,43 @@
-public class Event extends Task {
-    private final String from;
-    private final String to;
 
-    public Event(String description, String from, String to) {
+public class Event extends Task {
+    protected String startTime;
+    protected String endTime;
+
+    /**
+     * Constructs an Event task.
+     *
+     * @param description Description of the event.
+     * @param startTime Event start time.
+     * @param endTime Event end time.
+     */
+    public Event(String description, String startTime, String endTime) {
         super(description);
-        this.from = from;
-        this.to = to;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    /**
+     * Constructs an Event task with a completion status.
+     * Used when loading from a file.
+     *
+     * @param description Description of the event.
+     * @param timeInfo Time information string (format: "start/end").
+     * @param isDone Whether the task is completed.
+     */
+    public Event(String description, String timeInfo, boolean isDone) {
+        super(description, isDone);
+        String[] times = timeInfo.split("/");
+        this.startTime = times[0];
+        this.endTime = times.length > 1 ? times[1] : "";
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        return "[E]" + super.toString() + " (from: " + startTime + " to: " + endTime + ")";
+    }
+
+    @Override
+    public String toFileFormat() {
+        return "E | " + (isDone ? "1" : "0") + " | " + description + " | " + startTime + "/" + endTime;
     }
 }
