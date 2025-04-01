b34806132104b91408c513dec069ee9acbce6a22
Himeth Thewmika <142070705+himethcodes@users.noreply.github.com>
2025-04-01 18:10:39 +0800

    Update ListCommand.java

---
+++ ip/src/main/java/ListCommand.java
@@ -0,0 +1,10 @@
+/**
+ * Command to list all tasks.
+ */
+
+public class ListCommand extends Command {
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) {
+        ui.showTasks(tasks);
+    }
+}
