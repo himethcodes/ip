515bb1e19bd0321b4309d84efe40704c6afa2ede
Himeth Thewmika <142070705+himethcodes@users.noreply.github.com>
2025-04-01 15:43:33 +0800

    Update BuddyException.java

--- ip/src/main/java/BuddyException.java
+++ ip/src/main/java/BuddyException.java
@@ -1,3 +1,7 @@
+/**
+ * Custom exception for handling specific errors in the application.
+ */
+
 public class BuddyException extends Exception {
     public BuddyException(String message) {
         super(message);
